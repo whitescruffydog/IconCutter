ce30cfbfc845f148c3b81a44c5eabb3ab3598257
Jessica Mesler <JLM348@pitt.edu>
2019-05-24 20:04:34 -0400

    Added anti-overwrite check
    Also lowered compiler compliance level

--- makeicon.java
+++ makeicon.java
@@ -7,18 +7,13 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
 import java.awt.AWTException;
-import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException; 
 import javax.imageio.ImageIO;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.util.Random;
 import java.awt.Graphics;
@@ -30,7 +25,7 @@ import java.awt.event.MouseMotionAdapter;
 public class makeicon{
 	static JFrame frame = new JFrame();
 	
-	public static void main (String args[])
+	public static void main (String[] args)
 	{
         Toolkit.getDefaultToolkit().addAWTEventListener(
           new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
@@ -55,11 +50,18 @@ public class makeicon{
 	private static void takeScreenshot(int x, int y)
 	{
        try {
-		   Random rand = new Random();
-		   int n = rand.nextInt(200000000);
+    	   boolean exists = true;
+    	   String fileName;
 		   
            Robot robot = new Robot();
-           String fileName = "./" + Integer.toString(n) + ".png";
+    	   do {
+			   Random rand = new Random();
+			   int n = rand.nextInt(200000000);
+
+	           fileName = "./IconMaker" + Integer.toString(n) + ".png";
+	           File tmpDir = new File(fileName);
+	           exists = tmpDir.exists();
+    	   }while(exists);
  
            // Define an area of size 500*400 starting at coordinates (10,50)
            Rectangle rectArea = new Rectangle(x-50, y-50, 100, 100);
@@ -73,39 +75,40 @@ public class makeicon{
     }
 
 }
-		// Create a component that you can actually draw on.
-	   class DrawPane extends JPanel {
-		   Point drawRect = new Point(0, 0);
-		   public DrawPane(){
-			   setOpaque(false);
-			   addMouseMotionListener(new MouseMotionAdapter() {
-                public void mouseMoved(MouseEvent e) {
-                    drawRect = e.getPoint();
-					repaint();
-                }
-				});
-		   }
-			public void paintComponent(Graphics g) {
-				//g.fillRect(20, 20, 100, 200); // Draw on g here e.g.
-				g.drawRect((int)drawRect.getX()-51, (int)drawRect.getY()-51, 102, 102);
-			}
-			
+// Create a component that you can actually draw on.
+   class DrawPane extends JPanel {
+	   Point drawRect = new Point(0, 0);
+	   
+	   public DrawPane(){
+		   setOpaque(false);
+		   addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseMoved(MouseEvent e) {
+                drawRect = e.getPoint();
+				repaint();
+            }
+			});
 	   }
-
-	class MKeyListener extends KeyAdapter {
-	 
-		@Override
-		public void keyPressed(KeyEvent event) {
-	 
-			char ch = event.getKeyChar();
-			 
-			if (ch == 'a' ||ch == 'b'||ch == 'c' ||ch == 'q' ) {
-				System.exit(0);
-			}
-			if( ch == 'm')
-			{
-				makeicon.frame.setState(JFrame.ICONIFIED);
-			}
+	   
+		public void paintComponent(Graphics g) {
+			g.drawRect((int)drawRect.getX()-51, (int)drawRect.getY()-51, 102, 102);
 		}
-	}
+		
+   }
+
+class MKeyListener extends KeyAdapter {
+ 
+	@Override
+	public void keyPressed(KeyEvent event) {
+ 
+		char ch = event.getKeyChar();
+		 
+		if (ch == 'a' ||ch == 'b'||ch == 'c' ||ch == 'q' ) {
+			System.exit(0);
+		}
+		if( ch == 'm')
+				{
+					makeicon.frame.setState(JFrame.ICONIFIED);
+				}
+			}
+}
 
