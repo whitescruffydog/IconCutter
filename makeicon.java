cea00fd7837f49fb258333159bffc9ed34a427a2
Jessica Mesler <JLM348@pitt.edu>
2019-05-30 16:35:03 -0400

    Added icon size choice
    Also updated how naming pictures works.

--- makeicon.java
+++ makeicon.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
@@ -15,7 +16,6 @@ import java.io.File;
 import java.io.IOException; 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-import java.util.Random;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseMotionAdapter;
@@ -24,9 +24,40 @@ import java.awt.event.MouseMotionAdapter;
 
 public class makeicon{
 	static JFrame frame = new JFrame();
+	static int size = 100;
 	
 	public static void main (String[] args)
 	{
+		JFrame frame2 = new JFrame();
+		
+		String s = (String)JOptionPane.showInputDialog(
+                frame2,
+                "Enter the size (in pixels) of one side of the icons...\nOdd numbers will be made even.",
+                "IconMaker",
+                JOptionPane.PLAIN_MESSAGE,
+                null, null, "100");
+		if (s == null)
+		{
+			System.exit(0);
+		}
+		
+		if (s.matches("-?\\d+")) {
+			size = Integer.parseInt(s);
+			if (size % 2 == 1){
+				size ++;
+			}
+		}
+		
+		System.out.println(size);
+
+		//If a string was returned, say so.
+//		if ((s != null) && (s.length() > 0)) {
+//		setLabel("Green eggs and... " + s + "!");
+//		return;
+//		}
+		
+		
+		
         Toolkit.getDefaultToolkit().addAWTEventListener(
           new Listener(), AWTEvent.MOUSE_EVENT_MASK | AWTEvent.FOCUS_EVENT_MASK);
 		frame.addKeyListener(new MKeyListener());
@@ -54,9 +85,9 @@ public class makeicon{
     	   String fileName;
 		   
            Robot robot = new Robot();
+           int n = 0;
     	   do {
-			   Random rand = new Random();
-			   int n = rand.nextInt(200000000);
+			   n++;
 
 	           fileName = "./IconMaker" + Integer.toString(n) + ".png";
 	           File tmpDir = new File(fileName);
@@ -64,7 +95,7 @@ public class makeicon{
     	   }while(exists);
  
            // Define an area of size 500*400 starting at coordinates (10,50)
-           Rectangle rectArea = new Rectangle(x-50, y-50, 100, 100);
+           Rectangle rectArea = new Rectangle(x-(makeicon.size/2), y-(makeicon.size/2), makeicon.size, makeicon.size);
            BufferedImage screenFullImage = robot.createScreenCapture(rectArea);
            ImageIO.write(screenFullImage, "png", new File(fileName));
 		   System.out.println("Screenshot taken.");
@@ -90,7 +121,7 @@ public class makeicon{
 	   }
 	   
 		public void paintComponent(Graphics g) {
-			g.drawRect((int)drawRect.getX()-51, (int)drawRect.getY()-51, 102, 102);
+			g.drawRect((int)drawRect.getX()-(makeicon.size/2+1), (int)drawRect.getY()-(makeicon.size/2+1), makeicon.size+2, makeicon.size+2);
 		}
 		
    }
